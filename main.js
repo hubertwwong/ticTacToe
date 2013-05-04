c53bda56e5478b72ee549f821d19d8ec1152410c
Hubert Wong <hubertwwong@gmail.com>
2013-05-03 17:05:26 -0700

    Added some colors borders. but probably want to refactor it so its a simpler. the win conditions.

--- main.js
+++ main.js
@@ -4,6 +4,41 @@ var engine = function() {
 	var turn = 0;
 	var win = 0;
  	
+ 	// CSS FUNCTIONS
+ 	// =======================================================================
+ 	
+ 	// change the text of the game cell.
+ 	var gameCellText = function(loc, value) {
+ 		var currentCell = document.getElementById("cell" + loc + "Btn");
+ 		currentCell.innerHTML = value;
+ 	};
+ 	
+ 	// change the game cell value.
+ 	// 5 values X, O, XW, OW, N
+ 	var gameCellFormat = function(loc, value) {
+ 		var currentCell = document.getElementById("cell" + loc + "Btn");
+ 		
+ 		// check if its an X or O
+ 		if (value === "X") {
+ 			currentCell.className = "gameCellX";
+ 		}
+ 		else if (value === "O") {
+ 			currentCell.className = "gameCellO";
+ 		}
+ 		if (value === "XW") {
+ 			currentCell.className = "gameCellXWin";
+ 		}
+ 		else if (value === "OW") {
+ 			currentCell.className = "gameCellOWin";
+ 		}
+ 		else if (value === "N") {
+ 			currentCell.className = "gameCell";
+ 		}
+ 	}
+ 	
+ 	// GAME FUNCTIONS
+ 	// =======================================================================
+ 	
  	// figures out the current player.
  	// either X or O. and returns the int.
  	// 1 for X
@@ -14,7 +49,7 @@ var engine = function() {
  			cellValue = 2;
  		}
  		return cellValue;
- 	}
+ 	};
  	
  	// returns if you are an X or O
  	var currentPlayerText = function() {
@@ -34,29 +69,59 @@ var engine = function() {
  	var winning = function() {
  		// check who's turn it is.
  		var cellValue = currentPlayerInt();
+ 		var winFlag = false;
+ 		var c1 = -1;
+ 		var c2 = -1;
+ 		var c3 = -1;
  		
  		// horizontal
- 		for(var i=0 ; i<3 ; i++) {
- 			if(state[0 + 3*i] === cellValue && state[1 + 3*i] === cellValue && state[2 + 3*i] === cellValue) {
- 				win = cellValue;
- 				break;
- 			}
- 		}
+ 		if (winFlag === false) {
+	 		for(var i=0 ; i<3 ; i++) {
+	 			c1 = 0 + 3*i;
+	 			c2 = 1 + 3*i;
+	 			c3 = 2 + 3*i;
+	 			if(state[c1] === cellValue && state[c2] === cellValue && state[c3] === cellValue) {
+	 				win = cellValue;
+	 				gameCellFormat([c1, c2, c3], getCellText() + "Win");
+	 				break;
+	 			}
+	 		}
+	 	}
  		// vertical
- 		for(var i=0 ; i<3 ; i++) {
- 			if(state[0 + i] === cellValue && state[3 + i] === cellValue && state[6 + i] === cellValue) {
- 				win = cellValue;
- 				break;
- 			}
+ 		if (winFlag === false) {
+	 		for(var i=0 ; i<3 ; i++) {
+	 			c1 = 0 + i;
+	 			c2 = 3 + i;
+	 			c3 = 6 + i;
+	 			if(state[c1] === cellValue && state[c2] === cellValue && state[c3] === cellValue) {
+	 				win = cellValue;
+	 				gameCellFormat([c1, c2, c3], getCellText() + "Win");
+	 				break;
+	 			}
+	 		}
  		}
  		
  		// diagionals
- 		if(state[0] === cellValue && state[4] === cellValue && state[8] === cellValue) {
-			win = cellValue;
+ 		if (winFlag === false) {
+ 			c1 = 0;
+ 			c2 = 3;
+ 			c3 = 6;
+	 		if(state[c1] === cellValue && state[c2] === cellValue && state[c3] === cellValue) {
+				win = cellValue;
+				winFlag = true;
+			}
 		}
-		if(state[2] === cellValue && state[4] === cellValue && state[6] === cellValue) {
-			win = cellValue;
+		if (winFlag === false) {
+			c1 = 2;
+ 			c2 = 4;
+ 			c3 = 6;
+			if(state[c1] === cellValue && state[c2] === cellValue && state[c3] === cellValue) {
+				win = cellValue;
+				winFlag = true;
+			}
 		}
+		
+		// update with winflag
  	}
  		
 	return {
@@ -71,8 +136,10 @@ var engine = function() {
 				// update game state.
 				state[value]=cellValue;
 				
-				// update buttons...
-				document.getElementById("cell" + value + "Btn").innerHTML = currentPlayerText();
+				// update buttons text.
+				gameCellText(value, currentPlayerText());
+				gameCellFormat(value, currentPlayerText());
+				con.println(currentPlayerText() + " " + turn);
 				
 				// check to see if you are the winner
 				winning();
